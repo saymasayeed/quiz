920bf13767454b5543183f2473fc68283898bc5e
saymasayeed <54811316+saymasayeed@users.noreply.github.com>
2023-10-23 13:51:00 +0530

    Create Equal.c
    C code to determine whether two values are equal or not without using using arithmetic or comparison operators

---
+++ Equal.c
@@ -0,0 +1,11 @@
+#include<stdio.h>  
+int main ()  
+{     
+int a = 12;  
+int b = 12;  
+if ( !( a ^ b ) )  
+    printf ( " a is equal to b" );  
+else  
+    printf ( " a is not equal to b " );  
+return 0;  
+}  
